66646704350cf6e346cac660291d96edcacd04bc
Cesar Daniel (Dani) <rodriguez.cesar725@gmail.com>
2026-04-17 16:55:51 -0400

    Se actualiza la page de about y se modifican los archivos y se agregan nuevos archivos.

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,15 +1,19 @@
-    import AboutHero from '../src/components/About/AboutHero/AboutHero';
-    import NuestraHistoria from '../src/components/About/NuestraHistoria/NuestraHistoria';
-    import Mision from '../src/components/About/AboutMision/AboutMision';
+import AboutHero from '../src/components/About/AboutHero.jsx';
+import AboutHighlight from '../src/components/About/AboutHighlight.jsx';
+import AboutRoles from '../src/components/About/AboutRoles.jsx';
+import AboutCulture from '../src/components/About/AboutCulture.jsx';
+import AboutPurpose from '../src/components/About/AboutPurpose.jsx';
 
-    const About = () => {
+const About = () => {
     return (
         <>
-        <AboutHero />
-        <NuestraHistoria />
-        <Mision />
+            <AboutHero />
+            <AboutHighlight />
+            <AboutRoles />
+            <AboutCulture />
+            <AboutPurpose />
         </>
     );
-    };
+};
 
-    export default About;
+export default About;
---
+++ src/components/About/AboutCulture.jsx
@@ -0,0 +1,57 @@
+import { Container, Row, Col, Card } from "react-bootstrap";
+import style from "./AboutCulture.module.css";
+
+const AboutCulture = () => {
+    return (
+        <section className="py-5">
+            <Container>
+                <Row>
+                    <Col md={6} className="mb-3">
+                        <Card className={style.card}>
+                            <div className={style.image}>X - foto</div>
+
+                            <Card.Body>
+                                <h6 className={style.title}>Mirada interdisciplinaria</h6>
+                                <p className={style.text}>Aportamos distintas habilidades y perspectivas para resolver juntos los desafios complejos.</p>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={6} className="mb-3">
+                        <Card className={style.card}>
+                            <div className={style.image}>X - foto</div>
+
+                            <Card.Body>
+                                <h6 className={style.title}>Comunicación</h6>
+                                <p className={style.text}>Cultivamos el diálogo y la transparencia para tomar mejores desiciones como equipo.</p>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={6} className="mb-3">
+                        <Card className={style.card}>
+                            <div className={style.image}>X - foto</div>
+
+                            <Card.Body>
+                                <h6 className={style.title}>Mirada interdisciplinaria</h6>
+                                <p className={style.text}>Aprendemos, iteramos y mejoramos constantemente nuestros procesos y servicios.</p>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={6} className="mb-3">
+                        <Card className={style.card}>
+                            <div className={style.image}>X - foto</div>
+
+                            <Card.Body>
+                                <h6 className={style.title}>Vínculos</h6>
+                                <p className={style.text}>Aportamos distintas habilidades y perspectivas para resolver juntos desafiós complejos.</p>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+
+            </Container>
+        </section>
+
+    );
+};
+
+export default AboutCulture;
