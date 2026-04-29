6cb4a0c9e01545ab123b0e859d8b9bf0a04e85e1
xDani-R <rodriguez.cesar725@gmail.com>
2026-04-29 13:35:40 -0400

    Se actualiza la page de about con los nuevos cambios indicado en la plantilla.

--- frontend/src/components/About/AboutHero.jsx
+++ frontend/src/components/About/AboutHero.jsx
@@ -1,38 +1,46 @@
-import { Container, Row, Col } from "react-bootstrap";
-import styles from "./AboutHero.module.css";
-import teamImg from "../../assets/img/team.png"
+import { Container } from 'react-bootstrap';
+import styles from '../../components/About/AboutHero.module.css';
+import teamImg from '../../assets/img/About/about1.jpg';
+// Importamos los nuevos iconos
+import iconImpulsar from '../../assets/img/About/icon/impulsar 1.png';
+import iconPromocion from '../../assets/img/About/icon/promocion 1.png';
+import iconObjetivo from '../../assets/img/About/icon/objetivo 1.png';
 
 const AboutHero = () => {
-    return (
-        <section className={styles.hero}>
-            <Container>
-                <Row className="align-items-center">
+  return (
+    <section className={styles.hero}>
+      <Container fluid className={styles.container}>
+        <p className={styles.topLabel}>Quiénes somos</p>
+        <h1 className={styles.title}>El equipo que activa WorkHub</h1>
+        
+        <div className={styles.iconBar}>
+          <div className={styles.iconItem}>
+            <img src={iconImpulsar} alt="Impulsar" />
+            <span>Impulsamos talento</span>
+          </div>
+          <div className={styles.divider}>|</div>
+          <div className={styles.iconItem}>
+            <img src={iconPromocion} alt="Promocion" />
+            <span>Fomentamos comunidad</span>
+          </div>
+          <div className={styles.divider}>|</div>
+          <div className={styles.iconItem}>
+            <img src={iconObjetivo} alt="Objetivo" />
+            <span>Creamos impacto</span>
+          </div>
+        </div>
 
-                    <Col md={6}>
-                    <p className={styles.tag}>QUIÉNES SOMOS</p>
+        <p className={styles.description}>
+          Somos un grupo interdisciplinario que conecta oficios, creatividad y colaboración 
+          para hacer crecer la comunidad desde la práctica.
+        </p>
 
-                    <h1 className={styles.title}> 
-                        El equipo que activa WorkHub
-                    </h1>
-
-                    <p className={styles.text}>
-                        Somos un grupo interdiciplinario que conecta oficios, creatividad y colaboración para hacer crecer la comunidad desde la práctica.
-                    </p>
-
-                    <div className={styles.features}>
-                        <span>Personas compromentidas</span>
-                        <span>Trabajo colaborativo</span>
-                        <span>Pasión</span>
-                    </div>
-                    </Col>
-
-                    <Col md={6}>
-                        <img src={teamImg} className={`img-fluid ${styles.image}`} />
-                    </Col>
-                </Row>
-            </Container>
-        </section>
-    );
+        <div className={styles.imageWrapper}>
+          <img src={teamImg} alt="Team WorkHub" className={styles.mainImage} />
+        </div>
+      </Container>
+    </section>
+  );
 };
 
 export default AboutHero;
