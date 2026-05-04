742ae3c8dcf495b71af5fdf57e8da06379f92bd3
Jefferson Cardenas <jefferson.cardenas@genstudents.org>
2026-05-04 09:16:35 -0400

    dejar solo ruta delete reservas en routes.js

--- backend/src/Routes/routes.js
+++ backend/src/Routes/routes.js
@@ -3,19 +3,8 @@ const router = express.Router();
 const Controllers = require('../controllers/controllers');
 const validateReserva = require('../Middlewares/validateReserva');
 
-// ── GET /espacios/ ───────────────────────────────────────
-// router.get('/espacios', Controllers.getEspacios);
-
-// ── POST /reservas ─────────────────────────────────────────────
-// router.post('/reservas', validateReserva, Controllers.createReserva);
-
-// ── PUT /reservas/:id ──────────────────────────────────────────
-// router.put('/reservas/:id', validateReserva, Controllers.updateReserva);
-
 // ── DELETE /reservas/:id ───────────────────────────────────────
 router.delete('/reservas/:id', Controllers.deleteReserva);
 
-// ── GET /reservas/ ───────────────────────────────────────
-// router.get('/reservas', Controllers.getReservas);
 
 module.exports = router;
